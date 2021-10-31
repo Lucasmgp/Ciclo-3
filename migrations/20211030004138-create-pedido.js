b2d5853c5b4e082f99785253e7327b0ba8d6e60c
Lucas Pereira <lucasmgpereira18@gmail.com>
2021-10-31 10:03:10 -0300

    M

--- migrations/20211030004138-create-pedido.js
+++ migrations/20211030004138-create-pedido.js
@@ -15,7 +15,7 @@ module.exports = {
         allowNull:false,
         type: Sequelize.INTEGER,
         references:{
-          model:'clientes',
+          model:'Clientes',
           key:'id'
         },
         onDelete:'CASCADE',
