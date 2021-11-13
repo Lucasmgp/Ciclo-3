5d34496df6d23f3d76e565d90c8d73bfbb5fe428
Lucas Pereira <lucasmgpereira18@gmail.com>
2021-11-13 08:55:28 -0300

    Desafio ciclo 3

--- Controller.js
+++ Controller.js
@@ -7,11 +7,15 @@ app.use(cors());
 app.use(express.json());
 
 const models=require('./models');
-const { application } = require('express');
+
 let cliente=models.Cliente;
 let itemPedido=models.ItemPedido;
 let pedido=models.Pedido;
 let servico=models.Servico;
+
+let compra= models.Compra;
+let produto= models.Produto;
+let itemcompra= models.ItemCompra;
 
 app.get('/', function(req,res){
     res.send('Olá mundo!')
@@ -279,6 +283,203 @@ app.get('/excluircliente/:id', async(req,res)=>{
         return res.status(400).json({
             erro:true,
             mensagem: 'Erro ao excluir cliente.'
+        });
+    });
+});
+//-------------------------------------------------------------------------------------------
+//Desafio ciclo 3
+//criar compra
+
+app.post('/compras', async(req,res)=>{
+    await compra.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: 'Compra cadastrada com sucesso!'
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: 'Não foi possível criar compra!!!'
+        });
+    });
+});
+
+//Criar produto
+
+app.post('/produtos', async(req,res)=>{
+    await produto.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: 'Produto criado com sucesso!'
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: 'Não foi possível criar produto!!!'
+        });
+    });
+});
+
+//criar itens compra
+
+app.post('/itemcompra', async(req,res)=>{
+    await itemcompra.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: 'Item cadastrado com sucesso!'
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: 'Não foi possível cadastrar item!!!'
+        });
+    });
+});
+//Fim de criar
+
+//listar compras
+app.get('listacompras', async(req,res)=>{
+    await compra.findAll({
+        raw: true
+    }).then(function(compra){
+        res.json({compra})
+    });
+});
+
+//Listar produtos
+app.get('/listaprodutos', async(req,res)=>{
+    await produto.findAll({
+        raw: true
+    }).then(function(produtos){
+        res.json({produtos})
+    });
+});
+
+//lista item compra
+app.get('/listaitemcompra', async(req,res)=>{
+    await itemcompra.findAll({
+        order: [['CompraId', ASC]]
+    }).then(function(item){
+        res.json({item})
+    });
+});
+
+
+//Atualização de compra
+app.put('/atualizacompra', async(req,res)=>{
+    await compra.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: true,
+            message: 'Erro na alteração!!!'
+        });
+    });
+});
+
+//atualiza compra por id
+app.put('/compra/:id/editaritem', async(req,res)=>{
+    const item={
+        quantidade: req.body.quantidade,
+        valor: req.body.valor
+    };
+
+    if(!await compra.findByPk(req.params.id)){
+        return res.status(400).json({
+            error: true,
+            message: 'Compra não encontrada!!!'
+        });
+    };
+
+    if(!await produto.findByPk(req.body.ProdutoId)){
+        return res.status(400).json({
+            error: true,
+            message: 'Produto não encontrado!!!'
+        });
+    };
+
+    await itemcompra.update(item, {
+        where: Sequelize.and({ProdutoId: req.body.ProdutoId},
+            {CompraId: req.params.id})
+    }).then(function(itens){
+        return res.json({
+            error: false,
+            message: 'Compra alterada com sucesso!',
+            itens
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+        error: true,
+        message: 'Não foi possível alterar!!!'
+        });
+    });
+});
+
+//atualização de produto
+app.put('/atualizaproduto', async(req,res)=>{
+    await produto.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: 'Erro na alteração!!!'
+        });
+    });
+});
+
+//excluir compra por id
+app.get('excluircompra/:id', async(req,res)=>{
+    await compra.destroy({
+        where: {id: req.params.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: 'Compra excluída com sucesso!'
+        });
+    }).cathc(function(){
+        return res.status(400).json({
+            error: true,
+            message: 'Erro ao excluir compra!!!'
+        });
+    });
+});
+
+//excluir produto por id
+app.get('/excluirproduto/:id', async(req,res)=>{
+    await produto.destroy({
+        where: {id: req.params.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: 'Produto excluido com sucesso!'
+        });
+    }).catch(function(){
+        return res.status(400).json({
+            error: true,
+            message: 'Erro ao excluir produto!!!'
+        });
+    });
+});
+
+//excluir item por id da compra
+app.get('/excluiritemcompra/:CompraId', async(req,res)=>{
+    await itemcompra.destroy({
+        where: {CompraId: req.params.CompraId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: 'Item excluído com sucesso!'
+        });
+    }).catch(function(){
+        return res.status(400).json({
+            error: true,
+            message: 'Erro ao excluir item!!!'
         });
     });
 });
